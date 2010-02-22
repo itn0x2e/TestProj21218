5ea794a0af7523b40c163b89075c660f7106f65d
ItamarG <int0x2e@gmail.com>
2010-02-22 19:18:44 +0200

    perliminary version of create_authentication

---
+++ constants.h
@@ -0,0 +1,9 @@
+#ifndef __CONSTANTS_H__
+#define __CONSTANTS_H__
+
+#define MAX_USERNAME_LEN (20)
+#define MAX_PASSWORD_LEN (32)
+
+#define MAX_LINE_LEN (MAX_USERNAME_LEN + MAX_PASSWORD_LEN + 2)
+
+#endif /* __CONSTANTS_H__ */
--- util.h
+++ util.h
@@ -2,13 +2,18 @@
 #ifndef __UTIL_H__
 #define __UTIL_H__
 
-#define ERROR(x) fprintf(stderr, "ERROR: %s: %s", __FUNCTION__, x)
-#define WARN(x) fprintf(stderr, "WARNING: %s: %s", __FUNCTION__, x)
-#define TRACE(x) fprintf(stderr, "TRACE: %s: %s", __FUNCTION__, x)
+
+#define ERROR(x) fprintf(stderr, "ERROR: %s: %s\n", __FUNCTION__, x)
+#define WARN(x) fprintf(stderr, "WARNING: %s: %s\n", __FUNCTION__, x)
+#define TRACE(x) fprintf(stderr, "TRACE: %s: %s\n", __FUNCTION__, x)
+
+#define FAIL(x) \
+		ERROR(x); \
+		goto LBL_ERROR;
 
 
 #define CHECK(x) if (!(x)) {  \
-			ERROR("Assertion failed: #x"); \
+			ERROR("Assertion failed: " #x); \
 			goto LBL_ERROR; \
 		 }
 
