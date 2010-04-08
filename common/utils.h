f3d293b750c7dbe95cad34afc4e6d66a1981d4fc
Tal Nordan <talnordan@localhost>
2010-04-08 17:54:23 +0300

    restored PERROR

--- common/utils.h
+++ common/utils.h
@@ -12,7 +12,7 @@
 #define MIN(a, b) (((a) > (b)) ? (b) : (a))
 #define MAX(a, b) (((a) > (b)) ? (a) : (b))
 
-
+#define PERROR() perror("Error")
 
 #ifdef DEBUG
 	#define TRACE(msg) fprintf(stderr, "TRACE: %s:%d (%s): %s\n", __FILE__, __LINE__, __FUNCTION__, msg)
