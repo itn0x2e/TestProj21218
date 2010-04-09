46de1b1715625bc31053136d57a831f87f67626e
Tal Nordan <talnordan@localhost>
2010-04-09 12:15:41 +0300

    added files

--- rainbow_table/my_rainbow_table.c
+++ rainbow_table/my_rainbow_table.c
@@ -1,11 +1,14 @@
 #include "my_rainbow_table.h"
 
 static DEHT * createEmptyRainbowTable();
+static LONG_INDEX_PROJ * createSeeds(DEHT * rainbowTable, ulong_t rainbowChainLen);     
 static bool_t fillRainbowTable(passwordEnumerator_t * passwordEnumerator,
 			       char * firstPass,
 			       BasicHashFunctionPtr cryptHashPtr,
 			       ulong_t chainLength,
 			       LONG_INDEX_PROJ * seeds);
+static void closeRainbowTable(DEHT * rainbowTable);
+
 static bool_t insertIntoDEHT(const byte_t * key, uint_t keyLen, const char * dataStr);
 
 bool_t RT_generate(passwordEnumerator_t * passwordEnumerator,
@@ -17,14 +20,29 @@ bool_t RT_generate(passwordEnumerator_t * passwordEnumerator,
 		   const char * hashTableFilePrefix,
 		   ulong_t nHashTableEntries,
 		   ulong_t nPairsPerBlock) {
-	char * firstPass = generatorPassword;
-	byte_t curHash[MAX_DIGEST_LEN];
+	bool_t ret = FALSE;
+	DEHT * rainbowTable = NULL;
+	LONG_INDEX_PROJ * seeds = NULL;
 	
-	createEmptyRainbowTable();
+	/* Create an empty rainbow table */
+	CHECK(rainbowTable = createEmptyRainbowTable());
 	
-	fillRainbowTable(passwordEnumerator, passwordGenerator, enumeratorPassword, generatorPassword, hashFunc, rainbowChainLen,
-			       LONG_INDEX_PROJ * seeds)
-		
+	/* Create seeds and store them in the rainbow table */
+	CHECK(seeds = generateSeeds(rainbowTable, rainbowChainLen));
+	
+	/* Fill the rainbow table */
+	ret = fillRainbowTable(passwordEnumerator,
+			       passwordGenerator,
+			       enumeratorPassword,
+			       generatorPassword,
+			       hashFunc,
+			       rainbowChainLen,
+			       seeds);
+			       
+LBL_ERROR:
+	closeRainbowTable(DEHT * rainbowTable);
+	FREE(seeds);
+	return ret;
 }
 
 RainbowTable_t * RT_open(const passwordGenerator_t * passGenerator,
@@ -37,7 +55,20 @@ void RT_close(RainbowTable_t * self);
 
 bool_t RT_query(RainbowTable_t * self, byte_t * hash, ulong_t hashLen, char * resPassword, ulong_t resPasswordLen);
 
-static DEHT * createEmptyRainbowTable() {
+static DEHT * createEmptyRainbowTable(const char *prefix,
+				      const char *dictName,
+				      int numEntriesInHashTable,
+				      int nPairsPerBlock,
+				      int nBytesPerKey,
+				      int nUserBytes) {
+	return create_empty_DEHT(const char *prefix,
+				 hashKeyIntoTableFunctionPtr hashfun,
+				 hashKeyforEfficientComparisonFunctionPtr validfun,
+				 const char *dictName,
+				 int numEntriesInHashTable,
+				 int nPairsPerBlock,
+				 int nBytesPerKey,
+				 int nUserBytes
 	/*!TODO: dummy*/
 }
 
@@ -70,4 +101,10 @@ static bool_t fillRainbowTable(passwordEnumerator_t * passwordEnumerator,
 	/* end */
 	}
 	return TRUE;
+}
+
+static void closeRainbowTable(DEHT * rainbowTable) {
+	if (NULL != rainbowTable) {
+		lock_DEHT_files(rainbowTable);
+	}
 }
