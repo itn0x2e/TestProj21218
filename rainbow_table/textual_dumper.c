5951e0683a612f37984df8ca033e5f2e67c01b19
ita <ita@optimus.(none)>
2010-04-10 11:06:45 +0300

    compiles with no warnings/errors under strict settings, but not tested. Pending addition of bucket information for DEHT enumeration

--- rainbow_table/textual_dumper.c
+++ rainbow_table/textual_dumper.c
@@ -3,7 +3,6 @@
 #include "../common/constants.h"
 #include "../common/utils.h"
 #include "../common/misc.h"
-#include "../common/io.h"
 #include "../common/types.h"
 #include "../DEHT/DEHT.h"
 #include "../DEHT/hash_funcs.h"
@@ -15,8 +14,8 @@
 typedef struct htEnumerationParams_s {
 	RainbowTable_t * rt;
 
-	FILE * fdPasswords;
-	FILE * fdChains;
+	FILE * passwordsFd;
+	FILE * chainsFd;
 } htEnumerationParams_t;
 
 static bool_t printSeeds(RainbowTable_t * rt, FILE * fd)
@@ -36,7 +35,7 @@ static bool_t printSeeds(RainbowTable_t * rt, FILE * fd)
 	CHECK(NULL != fd);
 
 	/* load user bytes buffer */
-	CHECK(DEHT_STATUS_FAIL != DEHT_readUserBytes(rt->hashTable, (void **) &(rt->config), &userBytesSize));
+	CHECK(DEHT_STATUS_FAIL != DEHT_getUserBytes(rt->hashTable, (byte_t **) &(rt->config), &userBytesSize));
 	CHECK(NULL != rt->config);
 	CHECK(userBytesSize == sizeof(RainbowTableConfig_t) + rt->config->chainLength * sizeof(RainbowSeed_t));
 
@@ -62,23 +61,57 @@ LBL_CLEANUP:
 }
 
 
-bool_t printAndCalcChain(RainbowTable_t * rt, FILE * outputFD,
+bool_t calcAndPrintChain(RainbowTable_t * rt, FILE * outputFd,
 			   char * password, ulong_t passwordLen,
 			   byte_t * hashOut, ulong_t hashOutLen)
 {
 	bool_t ret = FALSE;
 
-	byte_t verifiedHash[MAX_DIGEST_LEN];
+	byte_t curHash[MAX_DIGEST_LEN];
+	char hashStr[MAX_DIGEST_LEN * 2 + 1];
+	int hashLen = 0;
+	
+	ulong_t j = 0;
+	ulong_t numPossiblePasswords;
 
 	TRACE_FUNC_ENTRY();	
 
 	CHECK(NULL != rt);
+	CHECK(NULL != outputFd);
 	CHECK(NULL != password);
+	CHECK(NULL != hashOut);
+	
+	numPossiblePasswords = passwordGeneratorGetSize(rt->passGenerator);
+
+       /* first, print the initial password */
+        fprintf(outputFd, "%s", password);
+        
+        /* calculate its hash */
+        CHECK(NULL != rt->hashFunc);
+        hashLen = cryptHash(rt->hashFunc, password, curHash);
+        CHECK(0 != hashLen);
+        
+        /* print the hash (in hex) */
+        binary2hexa(curHash, hashLen, hashStr, sizeof(hashStr));
+        fprintf(outputFd, "\t%s", hashStr);
 
 
+	for (j = 0; j < rt->config->chainLength; ++j) {
+		/* k = pseudo-random-function with seed seed[j] and input curHash; */
+		RainbowSeed_t k = pseudo_random_function(curHash, hashLen, rt->config->seeds[j]); /*! TODO: yet to be implemented */
+	
+		/* curHash = cryptographic-hash(get_kth_password_64b(k,S)); */
+		k %= numPossiblePasswords;
+		passwordGeneratorCalculatePassword(rt->passGenerator, k, rt->password);
+		cryptHash(rt->hashFunc, rt->password, curHash);
+	
+		fprintf(outputFd, "\t%s", rt->password);
+		binary2hexa(curHash, hashLen, hashStr, sizeof(hashStr));
+		fprintf(outputFd, "\t%s", hashStr);
+	}
 
 	/* copy result hash to user */
-	memcpy(hashOut, verifiedHash, MIN(sizeof(verifiedHash), hashOutLen));
+	memcpy(hashOut, curHash, MIN(sizeof(curHash), hashOutLen));
 
 	ret = TRUE;
 	goto LBL_CLEANUP;
@@ -100,6 +133,8 @@ static void hashTableEnumerationFunc(byte_t * key, ulong_t keySize,
 				     void * params)
 {
 	htEnumerationParams_t * enumerationParams = params;
+	byte_t verifiedHash[MAX_DIGEST_LEN];
+	byte_t verifiedValidationKey[BYTES_PER_KEY];
 
 	TRACE_FUNC_ENTRY();
 
@@ -107,13 +142,27 @@ static void hashTableEnumerationFunc(byte_t * key, ulong_t keySize,
 	CHECK(NULL != data);
 	CHECK(NULL != params);
 
-	CHECK(NULL != enumerationParams->fdPasswords);
-	CHECK(NULL != enumerationParams->fdChains);
+	CHECK(NULL != enumerationParams->rt);
+	CHECK(NULL != enumerationParams->passwordsFd);
+	CHECK(NULL != enumerationParams->chainsFd);
 
 	
-	fprintf(enumerationParams->fdPasswords, "%s\n", data);
+	fprintf(enumerationParams->passwordsFd, "%s\n", data);
 
-	fprintf(enumerationParams->fdChains, "%s\n", "would have printed the chain");
+
+	CHECK(calcAndPrintChain(enumerationParams->rt, enumerationParams->chainsFd,
+				(char *) data, dataLen,
+				verifiedHash, sizeof(verifiedHash)));
+
+	/* calculate this hash's validation key */
+	(void) DEHT_keyToValidationKeyHasher64(verifiedHash, getHashFunDigestLength(enumerationParams->rt->hashFunc), verifiedValidationKey);
+
+	if (0 != memcmp(verifiedValidationKey, key, keySize)) {
+		fprintf(enumerationParams->chainsFd, "Error: when begin with %s get wrong chain\n", data);
+	}
+
+	goto LBL_CLEANUP;
+
 
 LBL_ERROR:
 	TRACE_FUNC_ERROR();
@@ -162,8 +211,8 @@ bool_t RT_print(FILE * seedsAndPasswordsFd,
 	/* set up enumeration params */
 	memset(&enumerationParams, 0, sizeof(enumerationParams));
 	enumerationParams.rt = rt;
-	enumerationParams.fdPasswords = seedsAndPasswordsFd;
-	enumerationParams.fdChains = chainsFd;
+	enumerationParams.passwordsFd = seedsAndPasswordsFd;
+	enumerationParams.chainsFd = chainsFd;
 
 	/* scan all chains */	
 	CHECK(DEHT_enumerate(rt->hashTable, hashTableEnumerationFunc, (void *) &enumerationParams));
