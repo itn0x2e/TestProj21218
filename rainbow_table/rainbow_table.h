809ffec6bf7e64c2eb6279d455edcbbcf032842e
ita <ita@optimus.(none)>
2010-04-10 09:53:58 +0300

    draft of RT_print()

--- rainbow_table/rainbow_table.h
+++ rainbow_table/rainbow_table.h
@@ -49,4 +49,13 @@ void RT_close(RainbowTable_t * self);
 
 bool_t RT_query(RainbowTable_t * self, const byte_t * hash, ulong_t hashLen, bool_t * found);
 
+
+bool_t RT_print(FILE * seedsAndPasswordsFd,
+		FILE * chainsFd,
+
+		const passwordGenerator_t * passGenerator,
+		char * generatorPassword,
+		ulong_t passwordMaxLen,
+		const char * hashTableFilePrefix);
+
 #endif /* __RAINBOW_TABLE_H__ */
