4f94dcae6bac286db0ba435243b6efbcbeaa48c7
ita <ita@optimus.(none)>
2010-04-11 21:02:57 +0300

    removed unused types ulonglong_t and algorithmId_t

--- common/types.h
+++ common/types.h
@@ -4,17 +4,11 @@
 typedef unsigned char byte_t;
 typedef unsigned int uint_t;
 typedef unsigned long ulong_t;
-/*! typedef unsigned long long ulonglong_t; !*/
 
 typedef enum bool_e {
 	FALSE = 0,
 	TRUE
 } bool_t;
 
-typedef enum algorithmId_e {
-	ALGO_INVALID = 0,
-	ALGO_SHA1,
-	ALGO_MD5
-} algorithmId_t;
 
 #endif /* __TYPES_H__ */
